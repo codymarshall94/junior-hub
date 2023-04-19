b8d21010e783b8f81d02a911f5896943bb954461
Cody Marshall <codymstuntsx@gmail.com>
2023-04-19 13:52:33 -0700

    Added variants to add group-hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  variants: {
+    extend: {
+      display: ["group-hover"],
+    },
+  },
   theme: {
     extend: {
       fontFamily: {
