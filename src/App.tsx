7071c3e142a02c5ef7fa230365161c1ec4c2d087
Cody Marshall <codymstuntsx@gmail.com>
2023-04-19 16:08:44 -0700

    Provided routes to pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import './App.css'
+import Sidebar from "./pages/global/Sidebar";
+import CreateProject from "./pages/projects/CreateProject";
+import Projects from "./pages/projects/Projects";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="App flex">
+      <div>
+        <Sidebar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Projects />} />
+        <Route path="/create-project" element={<CreateProject />} />
+        <Route path="*" element={<h1>404</h1>} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
