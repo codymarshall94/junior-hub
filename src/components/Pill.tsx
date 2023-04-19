c08ee0cd8a49b08121c8ba0a0ec0644b6a3bd679
Cody Marshall <codymstuntsx@gmail.com>
2023-04-19 13:52:14 -0700

    Removed const colors variable

--- src/components/Pill.tsx
+++ src/components/Pill.tsx
@@ -1,23 +1,7 @@
-const pillColors = [
-  { name: "JavaScript", color: "#F7DF1E" },
-  { name: "TypeScript", color: "#3178C6" },
-  { name: "React", color: "#61DAFB" },
-  { name: "Vue", color: "#42B883" },
-  { name: "Angular", color: "#DD0031" },
-  { name: "Svelte", color: "#FF3E00" },
-  { name: "Node", color: "#339933" },
-  { name: "Python", color: "#3776AB" },
-];
-
-
 const Pill = ({ pill }: { pill: string }) => {
-  const pillColor = pillColors.find((color) => color.name === pill);
   return (
-    <div
-      className="rounded-full bg-[#F4F6F8] p-2 w-fit"
-      style={{ backgroundColor: pillColor?.color }}
-    >
-      <p className="text-sm font-bold">{pill}</p>
+    <div className="rounded bg-[#EFF3FC] p-2 w-fit">
+      <p className="text-sm">{pill}</p>
     </div>
   );
 };
