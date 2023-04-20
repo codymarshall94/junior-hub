f7600768d1a9e413ecfd18e849346299b546a9b1
Cody Marshall <codymstuntsx@gmail.com>
2023-04-20 15:25:47 -0700

    Added route for profile

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "./pages/global/Sidebar";
+import Profile from "./pages/profile/Profile";
 import CreateProject from "./pages/projects/CreateProject";
 import Projects from "./pages/projects/Projects";
 import { Routes, Route } from "react-router-dom";
@@ -11,6 +12,7 @@ function App() {
       </div>
       <Routes>
         <Route path="/" element={<Projects />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="/create-project" element={<CreateProject />} />
         <Route path="*" element={<h1>404</h1>} />
       </Routes>
