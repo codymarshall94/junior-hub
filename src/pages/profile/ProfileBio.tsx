d26ca91f2e6630b496f94e59e57933d4c53b46ef
Cody Marshall <codymstuntsx@gmail.com>
2023-04-21 17:17:37 -0700

    Added lorem placeholder

--- src/pages/profile/ProfileBio.tsx
+++ src/pages/profile/ProfileBio.tsx
@@ -1,9 +1,16 @@
 const ProfileBio = () => {
-    return (
-        <div>
-            <h1>Profile Bio</h1>
-        </div>
-    );
+  return (
+    <div className="p-4">
+      <h1 className="font-bold text-xl mb-4">Profile Bio</h1>
+      <p className="text-gray-500 w-1/2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam a
+        similique possimus est libero modi magnam explicabo quae voluptatibus
+        error, adipisci aperiam assumenda, veritatis necessitatibus illo
+        accusantium quo? Earum, repellendus.
+      </p>
+    </div>
+  );
 };
 
 export default ProfileBio;
--- src/pages/profile/ProfileExperience.tsx
+++ src/pages/profile/ProfileExperience.tsx
@@ -1,8 +1,15 @@
 const ProfileExperience = () => {
     return (
-        <div>
-            <h1>Profile Experience</h1>
-        </div>
+        <div className="p-4">
+      <h1 className="font-bold text-xl mb-4">Profile Experience</h1>
+      <p className="text-gray-500 w-1/2">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam a
+        similique possimus est libero modi magnam explicabo quae voluptatibus
+        error, adipisci aperiam assumenda, veritatis necessitatibus illo
+        accusantium quo? Earum, repellendus.
+      </p>
+    </div>
     );
 };
 
