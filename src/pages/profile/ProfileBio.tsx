5d7a5883e5cdcd2044b4d186fade1e9aac4db259
Cody Marshall <codymstuntsx@gmail.com>
2023-04-24 18:00:23 -0700

    Updated bio to remove lorem

--- src/pages/profile/ProfileBio.tsx
+++ src/pages/profile/ProfileBio.tsx
@@ -1,13 +1,13 @@
-const ProfileBio = () => {
+type ProfileBioProps = {
+  bio: string;
+};
+
+const ProfileBio = ({ bio }: ProfileBioProps) => {
   return (
     <div className="p-4">
       <h1 className="font-bold text-xl mb-4">Profile Bio</h1>
       <p className="text-gray-500 w-1/2">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam a
-        similique possimus est libero modi magnam explicabo quae voluptatibus
-        error, adipisci aperiam assumenda, veritatis necessitatibus illo
-        accusantium quo? Earum, repellendus.
+        {bio || "This user has not added a bio yet."}
       </p>
     </div>
   );
