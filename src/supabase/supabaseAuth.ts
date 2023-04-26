7be30e7e51770bdea543d0ccfb5939e16ae8747c
Cody Marshall <codymstuntsx@gmail.com>
2023-04-25 20:20:48 -0700

    Added new props to profiledata

--- src/supabase/supabaseAuth.ts
+++ src/supabase/supabaseAuth.ts
@@ -5,9 +5,11 @@ interface Credentials {
   password: string;
 }
 
-interface UserMetaData {
-  fullname: string;
+interface ProfileData {
+  full_name: string;
+  email: string;
   title: string;
+  avatar_url: string;
   bio: string;
   skills: string[];
   personalwebsite: string;
@@ -16,11 +18,13 @@ interface UserMetaData {
 }
 
 export const registerUser = async (credentials: Credentials) => {
+  console.log("registering user");
   const { data, error } = await supabase.auth.signUp(credentials);
   return { data, error };
 };
 
 export const loginUser = async (credentials: Credentials) => {
+  console.log("logging in user");
   const { data, error } = await supabase.auth.signInWithPassword(credentials);
   return { data, error };
 };
@@ -38,18 +42,35 @@ export const fetchUser = async () => {
   return user;
 };
 
-export const updateUser = async (info: UserMetaData) => {
-  const { fullname, title, bio, personalwebsite, linkedin, github, skills } = info;
-  const { data, error } = await supabase.auth.updateUser({
-    data: {
-      fullname,
+export const updateUser = async (info: ProfileData) => {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+  const {
+    full_name,
+    title,
+    email,
+    avatar_url,
+    bio,
+    personalwebsite,
+    linkedin,
+    github,
+    skills,
+  } = info;
+  console.log(user?.id + "user id");
+  const { data, error } = await supabase
+    .from("profiles")
+    .update({
+      full_name,
       title,
+      email,
+      avatar_url,
       bio,
-      skills,
-      personalwebsite,
+      website: personalwebsite,
       linkedin,
       github,
-    },
-  });
+      skills,
+    })
+    .eq("id", user?.id);
   return { data, error };
 };
