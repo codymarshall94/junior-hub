a33b431efbd930a5f4b6a2599d54dfb89c1a5f01
Cody Marshall <codymstuntsx@gmail.com>
2023-04-25 20:19:50 -0700

    Removed interface user and passed session

--- src/layouts/ProfileLayout.tsx
+++ src/layouts/ProfileLayout.tsx
@@ -1,19 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Profile from "../pages/profile/Profile";
 
-interface User {
-  email: string;
-  fullname: string;
-  ["user_metadata"]: {
-    fullname: string;
-    email: string;
-  };
-}
+const ProfileLayout = ({session}: {session: any}) => {
 
-const ProfileLayout = ({user}: {user: User}) => {
   return (
     <div className="flex flex-col space-y-4 w-full p-4">
-      <Profile user={user} />
+      <Profile session={session} />
       <Outlet />
     </div>
   );
