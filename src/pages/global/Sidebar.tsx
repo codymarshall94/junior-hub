f1945db67d329e79c76f7e668690bb09918bd49f
Cody Marshall <codymstuntsx@gmail.com>
2023-04-27 19:41:51 -0700

    Added notifications tab

--- src/pages/global/Sidebar.tsx
+++ src/pages/global/Sidebar.tsx
@@ -84,6 +84,26 @@ const links = [
     link: "/profile",
   },
   {
+    name: "Notifications",
+    svg: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M12 22s-8-4.5-8-10V6c0-3.5 4.5-6 8-6s8 2.5 8 6v6c0 5.5-8 10-8 10z"
+        />
+      </svg>
+    ),
+    link: "/notifications",
+  },
+  {
     name: "Settings",
     svg: (
       <svg
