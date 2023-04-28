b857f50b79c676a9dc3483449fbb5e7178bd04d5
Cody Marshall <codymstuntsx@gmail.com>
2023-04-27 19:41:15 -0700

    Removed profile component

--- src/layouts/ProfileLayout.tsx
+++ src/layouts/ProfileLayout.tsx
@@ -1,11 +1,8 @@
 import { Outlet } from "react-router-dom";
-import Profile from "../pages/profile/Profile";
 
-const ProfileLayout = ({session}: {session: any}) => {
-
+const ProfileLayout = () => {
   return (
     <div className="flex flex-col space-y-4 w-full p-4">
-      <Profile session={session} />
       <Outlet />
     </div>
   );
