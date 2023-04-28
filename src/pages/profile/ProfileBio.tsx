8bc2adb0bf8c53221450957ffc4db6500df349b4
Cody Marshall <codymstuntsx@gmail.com>
2023-04-27 19:42:41 -0700

    Removed useeffect for fetching profile

--- src/pages/profile/ProfileBio.tsx
+++ src/pages/profile/ProfileBio.tsx
@@ -1,47 +1,26 @@
-import { useEffect, useState } from "react";
-import { supabase } from "../../supabase/supabaseClient";
+interface ProfileBioProps {
+  bio: string;
+  website: string;
+  linkedin: string;
+  github: string;
+}
 
-const ProfileBio = ({ session }: { session: any }) => {
-  const [profile, setProfile] = useState<any>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const getProfile = async () => {
-      setLoading(true);
-      const { user } = session;
-      const { data, error } = await supabase
-        .from("profiles")
-        .select("*")
-        .eq("id", user.id)
-        .single();
-      if (error) {
-        console.warn(error);
-      } else if (data) {
-        setProfile(data);
-      }
-      setLoading(false);
-    };
-    getProfile();
-  }, [session]);
+const ProfileBio = ({ bio, website, linkedin, github }: ProfileBioProps) => {
 
   const links = [
     {
       name: "Personal Website",
-      link: profile?.website || "",
+      link: website || "",
     },
     {
       name: "LinkedIn",
-      link: profile?.linkedin || "",
+      link: linkedin || "",
     },
     {
       name: "GitHub",
-      link: profile?.github || "",
+      link: github || "",
     },
   ];
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
   return (
     <div className="p-4">
@@ -50,7 +29,7 @@ const ProfileBio = ({ session }: { session: any }) => {
         <div className="w-3/4">
           <h1 className="font-semibold text-lg mb-2">About Me</h1>
           <p className="text-gray-500 w-full">
-            {profile.bio || "This user has not added a bio yet."}
+            {bio || "This user has not added a bio yet."}
           </p>
         </div>
         <div className="border-l border-gray-200 pl-4 w-1/4">
