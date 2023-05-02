c7b3dc19ce782d158f591c4485cebd37bc6483ee
Cody Marshall <codymstuntsx@gmail.com>
2023-05-02 16:02:23 -0700

    Added a component to sort and align items

--- src/pages/projects/Projects.tsx
+++ src/pages/projects/Projects.tsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { supabase } from "../../supabase/supabaseClient";
-import ProjectItem from "./ProjectItem";
 import Project from "../../types/project";
 import ProjectsToolbar from "./ProjectsToolbar";
+import ProjectsGrid from "./ProjectsGrid";
 
 const MyProjects = ({ id }: { id: string }) => {
   const [projects, setProjects] = useState<Project[]>([]);
+  const [sortedProjects, setSortedProjects] = useState<Project[]>([]);
+  const [gridAllignment, setGridAllignment] = useState<"single" | "columns">(
+    "columns"
+  );
   const [loading, setLoading] = useState(true);
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
     getProjects();
@@ -34,11 +38,30 @@ const MyProjects = ({ id }: { id: string }) => {
     }
     if (!projectsData) return console.log("No data");
     setProjects(projectsData as any);
+    setSortedProjects(projectsData as any);
   };
 
   const handleProjectClick = (project: Project) => {
     navigate(`/projects/${project.id}`);
-    };
+  };
+
+  const handleSort = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const sortedProjects = [...projects];
+    if (e.target.value === "newest") {
+      sortedProjects.sort((a, b) => {
+        return (
+          new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+        );
+      });
+    } else if (e.target.value === "oldest") {
+      sortedProjects.sort((a, b) => {
+        return (
+          new Date(a.created_at).getTime() - new Date(b.created_at).getTime()
+        );
+      });
+    }
+    setSortedProjects(sortedProjects);
+  };
 
   return (
     <div className="flex flex-col w-screen items-center">
@@ -51,17 +74,18 @@ const MyProjects = ({ id }: { id: string }) => {
           Create Project
         </Link>
       </div>
-      <ProjectsToolbar length={projects.length} />
+      <ProjectsToolbar
+        length={projects.length}
+        gridAllignment={gridAllignment}
+        handleGridAllignment={setGridAllignment}
+        handleSort={handleSort}
+      />
       {loading ? (
         <div className="h-fit border-t-2 border-[#E0E6F7] w-full">
           <span>Fetching Projects...</span>
         </div>
       ) : (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 p-4 h-fit border-t-2 border-[#E0E6F7] w-full">
-          {projects.map((project) => (
-            <ProjectItem key={project["id"]} project={project} onClick={handleProjectClick} />
-          ))}
-        </div>
+        <ProjectsGrid gridAllignment={gridAllignment} projects={sortedProjects} onClick={handleProjectClick} />
       )}
     </div>
   );
