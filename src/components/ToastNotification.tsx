9821d2dc7aa7e98b47ea0d9f9b404b3a08d3e9ee
Cody Marshall <codymstuntsx@gmail.com>
2023-05-03 16:43:28 -0700

    Added switch case for types

--- src/components/ToastNotification.tsx
+++ src/components/ToastNotification.tsx
@@ -4,25 +4,52 @@ import "react-toastify/dist/ReactToastify.css";
 interface ToastNotificationProps {
   message: string;
   type: string;
-  onClose: () => void;
 }
 
 const ToastNotification = ({
   message,
   type,
-  onClose,
 }: ToastNotificationProps) => {
-  const save = () =>
-    toast.success("Profile Saved Succesfully!", {
-      position: "bottom-left",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-    });
+  switch (type) {
+    case "success":
+      toast.success(message, {
+        position: "bottom-left",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      break;
+    case "error":
+      toast.error(message, {
+        position: "bottom-left",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      break;
+    case "info":
+      toast.info(message, {
+        position: "bottom-left",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      break;
+    case "warn":
+      toast.warn(message, {
+        position: "bottom-left",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        draggable: true,
+      });
+      break;
+    default:
+      toast(message);
+  }
 
   return <ToastContainer />;
 };
