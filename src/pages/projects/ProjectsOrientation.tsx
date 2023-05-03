214e34c41b170eb08cf5ad3dfc687db2cf9fe943
Cody Marshall <codymstuntsx@gmail.com>
2023-05-03 16:44:42 -0700

    Added component to display grid orientations

---
+++ src/pages/projects/ProjectsOrientation.tsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+type OrientationProps = {
+  gridAllignment: "single" | "columns";
+  handleGridAllignment: (allignment: "single" | "columns") => void;
+};
+
+const ProjectsOrientation = ({
+  gridAllignment,
+  handleGridAllignment,
+}: OrientationProps) => {
+  return (
+    <div className="flex items-center">
+      <button
+        onClick={() => handleGridAllignment("single")}
+        className={`mr-2 border-2 border-[#E0E6F7] rounded p-1 ${
+          gridAllignment === "single" ? "bg-[#E0E6F7]" : ""
+        }`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+      <button
+        onClick={() => handleGridAllignment("columns")}
+        className={`mr-4 border-2 border-[#E0E6F7] rounded p-1 ${
+          gridAllignment === "columns" ? "bg-[#E0E6F7]" : ""
+        }`}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6A2.25 2.25 0 016 3.75h2.25A2.25 2.25 0 0110.5 6v2.25a2.25 2.25 0 01-2.25 2.25H6a2.25 2.25 0 01-2.25-2.25V6zM3.75 15.75A2.25 2.25 0 016 13.5h2.25a2.25 2.25 0 012.25 2.25V18a2.25 2.25 0 01-2.25 2.25H6A2.25 2.25 0 013.75 18v-2.25zM13.5 6a2.25 2.25 0 012.25-2.25H18A2.25 2.25 0 0120.25 6v2.25A2.25 2.25 0 0118 10.5h-2.25a2.25 2.25 0 01-2.25-2.25V6zM13.5 15.75a2.25 2.25 0 012.25-2.25H18a2.25 2.25 0 012.25 2.25V18A2.25 2.25 0 0118 20.25h-2.25A2.25 2.25 0 0113.5 18v-2.25z"
+          />
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default ProjectsOrientation;
