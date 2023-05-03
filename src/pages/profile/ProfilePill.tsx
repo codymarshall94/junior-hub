352b244cff178ce2528a48a87bb9dc2f3b4c00cc
Cody Marshall <codymstuntsx@gmail.com>
2023-05-03 16:42:54 -0700

    Removed

--- src/pages/profile/ProfilePill.tsx
+++
@@ -1,9 +0,0 @@
-const ProfilePill = () => {
-  return (
-    <div className="flex flex-row space-x-2 items-center bg-[#30AB4A] py-1 px-2 rounded-md">
-      <span className="text-sm text-white">Team Player</span>
-    </div>
-  );
-};
-
-export default ProfilePill;
