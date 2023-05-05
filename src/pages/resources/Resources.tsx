3ae62b32d5c262522ff9315e0416f01ef7176a8d
Cody Marshall <codymstuntsx@gmail.com>
2023-05-05 15:23:38 -0700

    Added template page

---
+++ src/pages/resources/Resources.tsx
@@ -0,0 +1,10 @@
+const Resources = () => {
+    return (
+      <div className="w-full h-full flex flex-col items-center justify-center">
+        <h1 className="text-4xl font-bold">Resources</h1>
+      </div>
+    );
+  };
+  
+  export default Resources;
+  
