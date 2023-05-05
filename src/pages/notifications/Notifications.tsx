7431f1a4074399c7ca15345b74c3e3757f49ee9d
Cody Marshall <codymstuntsx@gmail.com>
2023-05-05 15:11:01 -0700

    Added ternary values for notification type

--- src/pages/notifications/Notifications.tsx
+++ src/pages/notifications/Notifications.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getNotifications } from "../../supabase/supabaseUtils";
 import Notification from "../../types/notification";
 import NotificationHeader from "./NotificationHeader";
@@ -8,6 +9,8 @@ import Loading from "../../components/Loading";
 const Notifications = ({ id }: { id: string }) => {
   const [notifications, setNotifications] = useState<Notification[]>([]);
   const [loading, setLoading] = useState(true);
+
+  console.log(notifications);
 
   useEffect(() => {
     getNotifications(id)
@@ -26,11 +29,8 @@ const Notifications = ({ id }: { id: string }) => {
   }
 
   return (
-    <div className="flex flex-col space-y-4 p-4 w-1/2 border-2 border-gray-200 rounded-md mx-auto my-4">
-      <h1 className="text-2xl font-bold">
-        Notifications
-      </h1>
-
+    <div className="flex flex-col space-y-4 p-4 w-3/4 border-2 border-gray-200 rounded-md mx-auto my-4">
+      <h1 className="text-2xl font-bold">Notifications</h1>
       <div className="flex flex-col">
         {notifications.map((notification) => (
           <div
@@ -38,11 +38,23 @@ const Notifications = ({ id }: { id: string }) => {
             className="border-t-2 border-gray-200 py-4"
           >
             <NotificationHeader
-              name={notification.message}
+              message={notification.message}
               time={notification.created_at}
               id={notification.id}
+              avatar={notification.sender_avatar || ""}
+              senderId={notification.sender_id}
             />
-            <NotificationButtons notification={notification} />
+            {notification.type === "request_to_join" && (
+              <NotificationButtons notification={notification} />
+            )}
+            {notification.type === "accepted_request_to_join" && (
+              <Link
+                to={`/projects/${notification.project_id}`}
+                className="bg-[#1CE177] hover:bg-[#3C65F5]  text-white font-bold p-2 w-1/4 mt-4 rounded-md w-36 transition-all duration-300 ease-in-out"
+              >
+                View Project
+              </Link>
+            )}
           </div>
         ))}
       </div>
