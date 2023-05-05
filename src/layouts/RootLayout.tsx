e22164728375e514c72727d682b7793062249c06
Cody Marshall <codymstuntsx@gmail.com>
2023-05-05 10:19:20 -0700

    Changed to include navbar

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
-import Sidebar from "../pages/global/Sidebar";
+import Navbar from "../pages/global/Navbar";
 
 const RootLayout = ({profile}: any) => {
+
   return (
-    <div className="flex">
-      <Sidebar profile={profile} />
+    <div className="flex flex-col w-screen h-screen">
+      <Navbar profile={profile} />
+      <div className="flex flex-col w-1/2 h-full mx-auto">
       <Outlet />
+      </div>
     </div>
   );
 };
