be208f964276b6c6aec3b27132911a6838d87b64
Cody Marshall <codymstuntsx@gmail.com>
2023-05-05 10:18:22 -0700

    Created component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
